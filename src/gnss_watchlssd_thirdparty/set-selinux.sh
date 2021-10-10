9c0aa37724ff35acc3ad51d1668e260778744529
Iceows <mounierr07@gmail.com>
2021-10-10 15:26:36 +0200

    Update policy

--- src/gnss_watchlssd_thirdparty/set-selinux.sh
+++ src/gnss_watchlssd_thirdparty/set-selinux.sh
@@ -19,6 +19,13 @@ chcon -v u:object_r:system_lib_file:s0 /system/system_ext/lib64/libgnss_lss_gw_t
 # set context gnss_watchlssd_thirdparty
 echo "/system/xbin/gnss_watchlssd_thirdparty		u:object_r:hi110x_daemon_exec:s0" >> /system/etc/selinux/plat_file_contexts 
 
+echo "(allow system_app hi110x_socket (sock_file (write)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+echo "(allow system_app hi110x_daemon (unix_stream_socket (connectto)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+echo "(allow system_app hi110x_daemon (unix_dgram_socket (sendto)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+echo "(allow system_app hi110x_daemon (binder (transfer)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+
+#(allow hi110x_daemon system_data_root_file (dir (read)))
+#(allow hi110x_daemon device (chr_file (ioctl read getattr lock open)))
 
 # fix system ntp_server (europe pool)
 set global ntp_server europe.pool.ntp.org
