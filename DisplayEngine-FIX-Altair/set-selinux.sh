67e90758e7bc378e82596634157dbe35d8e21df0
Iceows <mounierr07@gmail.com>
2021-10-12 22:08:17 +0200

    Update 1.6

--- DisplayEngine-FIX-Altair/set-selinux.sh
+++ DisplayEngine-FIX-Altair/set-selinux.sh
@@ -1,10 +1,5 @@
 #!/sbin/sh
 
-# -rwxr-xr-x
-chmod 755 /system/bin/displayengineserver
-chown root:shell /system/bin/displayengineserver
-
-chcon -v u:object_r:displayengineserver_exec:s0 /system/bin/displayengineserver
 
 # -rw-r--r-- 1 root root u:object_r:system_file:s0  173376 2018-11-28 02:42 vendor.huawei.hardware.hwdisplay.displayengine@1.0.so
 
@@ -15,6 +10,13 @@ echo "(roletype object_r displayengine_hwservice)" >> /system/etc/selinux/plat_s
 echo "(typeattributeset displayengine_hwservice_26_0 (displayengine_hwservice))" >> /system/etc/selinux/mapping/26.0.cil
 
 # Add allow  for displayengine-hal-1.0
-echo "((allow hal_displayengine_default displayengine_hwservice (hwservice_manager (add find)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+# echo "(allow hal_displayengine_default displayengine_hwservice (hwservice_manager (add find)))" >> /vendor/etc/selinux/nonplat_sepolicy.cil
+
+# Add allow vendor.lineage.livedisplay
+echo "(allow system_server default_android_hwservice (hwservice_manager (find)))" >> /system/etc/selinux/plat_sepolicy.cil
+echo "(allow system_server default_android_service (service_manager (add)))" >> /system/etc/selinux/plat_sepolicy.cil
+echo "(allow system_server vendor_file (file (execute getattr map open read)))" >> /system/etc/selinux/plat_sepolicy.cil
+
+
 
 exit 0
