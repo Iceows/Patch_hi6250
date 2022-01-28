a2ccc522a73bd4db4464285467831b4fa37a4407
Iceows <mounierr07@gmail.com>
2022-01-28 08:51:08 +0100

    Add Dynamic root and safetynet option

---
+++ src/trebleapp/Misc.kt
@@ -0,0 +1,270 @@
+package me.phh.treble.app
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.display.DisplayManager
+import android.os.Parcel
+import android.os.ServiceManager
+import android.os.SystemProperties
+import android.preference.PreferenceManager
+import android.provider.Settings
+import android.util.Log
+import java.lang.ref.WeakReference
+
+import vendor.ims.zenmotion.V1_0.IZenMotion;
+
+@SuppressLint("StaticFieldLeak")
+object Misc: EntryStartup {
+    fun safeSetprop(key: String, value: String?) {
+        try {
+            Log.d("PHH", "Setting property $key to $value")
+            SystemProperties.set(key, value)
+        } catch (e: Exception) {
+            Log.d("PHH", "Failed setting prop $key", e)
+        }
+    }
+
+    val surfaceFlinger = ServiceManager.getService("SurfaceFlinger")
+    fun forceFps(v: Int) {
+        val data = Parcel.obtain()
+        try {
+            data.writeInterfaceToken("android.ui.ISurfaceComposer")
+            data.writeInt(v)
+            surfaceFlinger.transact(1035, data, null, 0)
+            Log.d("PHH", "Set surface flinger forced fps/mode to $v")
+        } catch (r: Exception) {
+            Log.d("PHH", "Failed setting surface flinger forced fps/mode to $v")
+        } finally {
+            data.recycle()
+        }
+    }
+
+    fun enableHwcOverlay(v: Boolean) {
+        val data = Parcel.obtain()
+        try {
+            data.writeInterfaceToken("android.ui.ISurfaceComposer")
+            data.writeInt(if(v) 0 else 1)
+            surfaceFlinger.transact(1008, data, null, 0)
+            Log.d("PHH", "Set surface flinger hwc overlay to $v")
+        } catch (r: Exception) {
+            Log.d("PHH", "Failed setting surface flinger hwc overlay to $v")
+        } finally {
+            data.recycle()
+        }
+    }
+
+    lateinit var ctxt: WeakReference<Context>
+    val spListener = SharedPreferences.OnSharedPreferenceChangeListener { sp, key ->
+        val c = ctxt.get()
+        if(c == null) return@OnSharedPreferenceChangeListener
+        val displayManager = c.getSystemService(DisplayManager::class.java)
+        when(key) {
+            MiscSettings.mobileSignal -> {
+                val value = sp.getString(key, "default")
+                SystemProperties.set("persist.sys.signal.level", value)
+                Log.d("PHH", "Setting signal level method to $value")
+            }
+            MiscSettings.fpsDivisor -> {
+                val value = sp.getString(key, "1")
+                Log.d("PHH", "Setting fps divisor to $value")
+                Settings.Global.putString(c.contentResolver, "fps_divisor", value)
+            }
+            MiscSettings.cameraTimestampOverride -> {
+                val value = sp.getString(key, "-1")
+                Log.d("PHH", "Setting cameraTimestampOverride to $value")
+                SystemProperties.set("persist.sys.phh.camera.force_timestampsource", value)
+            }
+            MiscSettings.maxAspectRatioPreO -> {
+                val value = sp.getString(key, "1.86")
+                SystemProperties.set("persist.sys.max_aspect_ratio.pre_o", value)
+                Log.d("PHH", "Setting max aspect ratio for pre-o app $value")
+            }
+            MiscSettings.multiCameras -> {
+                val value = sp.getBoolean(key, false)
+
+                safeSetprop("persist.sys.phh.include_all_cameras", if(value) "true" else "false")
+                if (value ||
+                        SystemProperties.get("vendor.camera.aux.packagelist", null) == null ||
+                        SystemProperties.get("camera.aux.packagelist", null) == null) {
+                    safeSetprop("vendor.camera.aux.packagelist", if (value) "nothing" else null)
+                    safeSetprop("camera.aux.packagelist", if (value) "nothing" else null)
+                    safeSetprop("ctl.restart", "vendor.camera-provider-2-4")
+                    safeSetprop("ctl.restart", "camera-provider-2-4")
+                    safeSetprop("ctl.restart", "cameraserver")
+                }
+            }
+            MiscSettings.forceCamera2APIHAL3 -> {
+                val value = sp.getBoolean(key, false)
+                val defValue = "0"
+                val newValue = if (value) "1" else defValue
+
+                if (value ||
+                        SystemProperties.get("persist.vendor.camera.HAL3.enabled", defValue) != newValue ||
+                        SystemProperties.get("persist.vendor.camera.eis.enable", defValue) != newValue) {
+                    safeSetprop("persist.vendor.camera.HAL3.enabled", newValue)
+                    safeSetprop("persist.vendor.camera.eis.enable", newValue)
+                    Log.d("PHH", "forced Camera2API HAL3 to $value")
+                    // Restart services
+                    safeSetprop("ctl.restart", "vendor.camera-provider-2-4")
+                    safeSetprop("ctl.restart", "camera-provider-2-4")
+                }
+            }
+            MiscSettings.headsetFix -> {
+                val value = sp.getBoolean(key, HuaweiSettings.enabled())
+                if (!sp.contains(key))
+                    Log.d("PHH", "Setting Huawei headset fix to $value")
+                if (value) {
+                    Log.d("PHH", "starting huaweiaudio")
+                    ForceHeadsetAudio.startup(c)
+                } else {
+                    Log.d("PHH", "stopping huaweiaudio")
+                    ForceHeadsetAudio.shutdown(c)
+                }
+            }
+            MiscSettings.roundedCorners -> {
+                val value = sp.getString(key, "-1").toInt()
+                if (value >= 0) {
+                    Settings.Secure.putInt(c.contentResolver, "sysui_rounded_content_padding", value)
+                    SystemProperties.set("persist.sys.phh.rounded_corners_padding", value.toString())
+                }
+            }
+            MiscSettings.roundedCornersOverlay -> {
+                val value = sp.getString(key, "-1").toFloat()
+                if (value >= 0) {
+                    Settings.Secure.putFloat(c.contentResolver, "sysui_rounded_size", value)
+                }
+            }
+            MiscSettings.linearBrightness -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.linear_brightness", if (value) "true" else "false")
+            }
+            MiscSettings.disableButtonsBacklight -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.disable_buttons_light", if (value) "true" else "false")
+            }
+            MiscSettings.forceNavbarOff -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.mainkeys", if (value) "1" else "0")
+            }
+            MiscSettings.bluetooth -> {
+                val value = sp.getString(key, "none")
+                android.util.Log.d("PHH", "Setting bluetooth workaround to $value")
+                when (value) {
+                    "none" -> {
+                        SystemProperties.set("persist.sys.bt.unsupport.features", "00000000")
+                        SystemProperties.set("persist.sys.bt.unsupport.states", "00000000")
+                        SystemProperties.set("persist.sys.bt.unsupport.stdfeatures", "00000000")
+                    }
+                    "mediatek" -> {
+                        SystemProperties.set("persist.sys.bt.unsupport.features", "00000000")
+                        SystemProperties.set("persist.sys.bt.unsupport.states", "00000000")
+                        SystemProperties.set("persist.sys.bt.unsupport.stdfeatures", "000001")
+                    }
+                    "huawei" -> {
+                        SystemProperties.set("persist.sys.bt.unsupport.features", "00000001")
+                        SystemProperties.set("persist.sys.bt.unsupport.states", "000000000000000000000011111")
+                        SystemProperties.set("persist.sys.bt.unsupport.stdfeatures", "000001")
+                    }
+                }
+            }
+            MiscSettings.displayFps -> {
+                val value = sp.getString(key, "-1").toInt()
+                val maxValue = displayManager.displays[0].supportedModes.size
+                if(value>= maxValue) {
+                    Log.d("PHH", "Trying to set impossible mode " + value)
+                } else {
+                    Log.d("PHH", "Trying to set mode " + value)
+                    forceFps(value)
+                }
+            }
+            MiscSettings.superUser -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.root", if (value) "true" else "false")
+            }
+            MiscSettings.safetyNet -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.safetynet", if (value) "true" else "false")
+            }
+            MiscSettings.pixelProps -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.pixelprops", if (value) "true" else "false")
+            }			
+            MiscSettings.remotectl -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.remote", if (value) "true" else "false")
+            }
+            MiscSettings.disableAudioEffects -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.disable_audio_effects", if (value) "1" else "0")
+            }
+            MiscSettings.forceA2dpOffloadDisable -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.disable_a2dp_offload", if (value) "1" else "0")
+            }
+            MiscSettings.noHwcomposer -> {
+                val value = sp.getBoolean(key, false)
+                enableHwcOverlay(!value)
+            }
+            MiscSettings.storageFUSE -> {
+                val value = sp.getBoolean(key, false)
+                Log.d("PHH", "Setting storageFUSE to $value")
+                SystemProperties.set("persist.sys.fflag.override.settings_fuse", if (!value) "true" else "false")
+            }
+            MiscSettings.backlightScale -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.backlight.scale", if (value) "1" else "0")
+            }
+            MiscSettings.headsetDevinput -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.overlay.devinputjack", if (value) "true" else "false")
+            }
+            MiscSettings.restartRil -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.phh.restart_ril", if (value) "true" else "false")
+            }
+            MiscSettings.minimalBrightness -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.overlay.minimal_brightness", if (value) "true" else "false")
+            }
+            MiscSettings.aod -> {
+                val value = sp.getBoolean(key, false)
+                SystemProperties.set("persist.sys.overlay.aod", if (value) "true" else "false")
+                OverlayPicker.setOverlayEnabled("me.phh.treble.overlay.misc.aod_systemui", value)
+            }
+            MiscSettings.dt2w -> {
+                // Let's try all known dt2w
+                val value = sp.getBoolean(key, false)
+                val asusSvc = try { IZenMotion.getService() } catch(e: Exception) { null }
+                if(asusSvc != null) {
+                    asusSvc.setDclickEnable(if(value) 1 else 0)
+                }
+            }
+        }
+    }
+
+    override fun startup(ctxt: Context) {
+        if (!MiscSettings.enabled()) return
+
+        val sp = PreferenceManager.getDefaultSharedPreferences(ctxt)
+        sp.registerOnSharedPreferenceChangeListener(spListener)
+
+        this.ctxt = WeakReference(ctxt.applicationContext)
+
+        //Refresh parameters on boot
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.fpsDivisor)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.cameraTimestampOverride)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.mobileSignal)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.maxAspectRatioPreO)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.multiCameras)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.forceCamera2APIHAL3)
+        if (! sp.contains(MiscSettings.headsetFix))
+            sp.edit().putBoolean(MiscSettings.headsetFix, HuaweiSettings.enabled()).commit()
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.headsetFix)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.bluetooth)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.displayFps)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.noHwcomposer)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.storageFUSE)
+        spListener.onSharedPreferenceChanged(sp, MiscSettings.dt2w)
+    }
+}
---
+++ src/trebleapp/MiscSettings.kt
@@ -0,0 +1,132 @@
+package me.phh.treble.app
+
+import android.app.AlertDialog
+import android.app.Application
+import android.hardware.display.DisplayManager
+import android.os.Bundle
+import android.util.Log
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+
+object MiscSettings : Settings {
+    val mobileSignal = "key_misc_mobile_signal"
+    val fpsDivisor = "key_misc_fps_divisor"
+    val displayFps = "key_misc_display_fps"
+    val maxAspectRatioPreO = "key_misc_max_aspect_ratio_pre_o"
+    val multiCameras = "key_misc_multi_camera"
+    val forceCamera2APIHAL3 = "key_misc_force_camera2api_hal3"
+    val headsetFix = "key_huawei_headset_fix"
+    val roundedCorners = "key_misc_rounded_corners"
+    val roundedCornersOverlay = "key_misc_rounded_corners_overlay"
+    val linearBrightness = "key_misc_linear_brightness"
+    val disableButtonsBacklight = "key_misc_disable_buttons_backlight"
+    val forceNavbarOff = "key_misc_force_navbar_off"
+    val bluetooth = "key_misc_bluetooth"
+    val superUser = "key_misc_superuser"
+    val safetyNet = "key_misc_safetynet"
+    val pixelProps = "key_misc_pixelprops"	
+    val securize = "key_misc_securize"
+    val removeTelephony = "key_misc_removetelephony"
+    val remotectl = "key_misc_remotectl"
+    val disableAudioEffects = "key_misc_disable_audio_effects"
+    val cameraTimestampOverride = "key_misc_camera_timestamp"
+    val forceA2dpOffloadDisable = "key_misc_force_a2dp_offload_disable"
+    val noHwcomposer = "key_misc_no_hwcomposer"
+    val storageFUSE = "key_misc_storage_fuse"
+    val backlightScale = "key_misc_backlight_scale"
+    val headsetDevinput = "key_misc_headset_devinput"
+    val restartRil = "key_misc_restart_ril"
+    val minimalBrightness = "key_misc_minimal_brightness"
+    val aod = "key_misc_aod"
+    val dt2w = "key_misc_dt2w"
+
+    override fun enabled() = true
+}
+
+class MiscSettingsFragment : SettingsFragment() {
+    override val preferencesResId = R.xml.pref_misc
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+
+        val securizePref = findPreference<Preference>(MiscSettings.securize)
+        securizePref!!.setOnPreferenceClickListener {
+                val builder = AlertDialog.Builder( this.getActivity() )
+                builder.setTitle(getString(R.string.remove_root))
+                builder.setMessage(getString(R.string.continue_question))
+
+                builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+
+                var cmds = listOf(
+                        "/sbin/su -c /system/bin/phh-securize.sh",
+                        "/system/xbin/su -c /system/bin/phh-securize.sh",
+                        "/system/xbin/phh-su -c /system/bin/phh-securize.sh",
+                        "/sbin/su 0 /system/bin/phh-securize.sh",
+                        "/system/xbin/su 0 /system/bin/phh-securize.sh",
+                        "/system/xbin/phh-su 0 /system/bin/phh-securize.sh"
+                )
+                for(cmd in cmds) {
+                    try {
+                        Runtime.getRuntime().exec(cmd).waitFor()
+                    } catch(t: Throwable) {}
+                }
+            }
+
+            builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            }
+
+            builder.show()
+            return@setOnPreferenceClickListener true
+        }
+
+        val removeTelephonyPref = findPreference<Preference>(MiscSettings.removeTelephony)
+        removeTelephonyPref!!.setOnPreferenceClickListener {
+
+            val builder = AlertDialog.Builder( this.getActivity() )
+            builder.setTitle(getString(R.string.remove_telephony_subsystem))
+            builder.setMessage(getString(R.string.continue_question))
+
+            builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+
+                var cmds = listOf(
+                        "/sbin/su -c /system/bin/remove-telephony.sh",
+                        "/system/xbin/su -c /system/bin/remove-telephony.sh",
+                        "/system/xbin/phh-su -c /system/bin/remove-telephony.sh",
+                        "/sbin/su 0 /system/bin/remove-telephony.sh",
+                        "/system/xbin/su 0 /system/bin/remove-telephony.sh",
+                        "/system/xbin/phh-su 0 /system/bin/remove-telephony.sh"
+                )
+                for(cmd in cmds) {
+                    try {
+                        Runtime.getRuntime().exec(cmd).waitFor()
+                    } catch(t: Throwable) {}
+                }
+            }
+
+            builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            }
+
+            builder.show()
+            return@setOnPreferenceClickListener true
+        }
+
+        val fpsPref = findPreference<ListPreference>(MiscSettings.displayFps)!!
+        val displayManager = activity.getSystemService(DisplayManager::class.java)
+        for(display in displayManager.displays) {
+            Log.d("PHH", "Got display $display")
+            for(mode in display.supportedModes) {
+                Log.d("PHH", "\tMode ${mode.modeId} $mode")
+            }
+        }
+
+        val fpsEntries = listOf("Don't force") + displayManager.displays[0].supportedModes.map {
+            val fps = it.refreshRate
+            val w = it.physicalWidth
+            val h = it.physicalHeight
+            "${w}x${h}@${fps}"
+        }
+        val fpsValues = listOf("-1") + displayManager.displays[0].supportedModes.map { (it.modeId - 1).toString() }
+
+        fpsPref.setEntries(fpsEntries.toTypedArray())
+        fpsPref.setEntryValues(fpsValues.toTypedArray())
+    }
+}
